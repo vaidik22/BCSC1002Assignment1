affd9a9472b081aceab258e05823ea38fda19c7f
vaidik22 <vaidik.nigam_cs19@gla.ac.in>
2020-08-30 13:35:18 +0530

    commit book class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,5 +6,67 @@
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
+    private String bookName;
+    private String bookAuthor;
+    private String bookISBN;
+
+    public Book() {
+        this.bookName = null;
+        this.bookAuthor = null;
+        this.bookISBN = null;
+    }
+
+    public Book(String bookName, String bookAuthorName, String isbnNumber) {
+        this.bookName = bookName;
+        this.bookAuthor = bookAuthorName;
+        this.bookISBN = isbnNumber;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthorName(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookISBN() {
+        return bookISBN;
+    }
+
+    public void setIsbnNumber(String bookISBN) {
+        this.bookISBN = bookISBN;
+    }
+
+    public String toString() {
+        return "Book Name" + getBookName() + "," +
+                "Authors Name" + getBookAuthor() + "," +
+                "ISBN Number" + getBookISBN() + ".";
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(bookName, book.bookName) &&
+                Objects.equals(bookAuthor, book.bookAuthor) &&
+                Objects.equals(bookISBN, book.bookISBN);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, bookAuthor, bookISBN);
+    }
+
 }
